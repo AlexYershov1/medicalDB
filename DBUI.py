e9aee164765e949dba7752eb46ad0396bd57f22c
AlexYershov1 <alexyershov1@gmail.com>
2021-11-21 17:46:41 +0200

    minor selection changes

--- DBUI.py
+++ DBUI.py
@@ -1,4 +1,5 @@
 import pyodbc
+import requests
 
 print ('Initiated program')
 
@@ -8,35 +9,42 @@ cursor = conn.cursor()
 print ('connection to DB established')
 
 
-def select(info):
+def select():
+    #ln =  input('Enter loinc-num ')
+    #firstName, lastName = input ('Enter patients\' name').split()
+    #date = input('Enter date')
+    #cursor.execute('SELECT LOINC\-NUM from medicalRecord where First name=? and Last name=? and Transaction time=?', firstName, lastName, date)
     return
 
-def history(info):
+def history():
     return
 
-def update(info):
+def update():
     return
 
-def delete(info):
+def delete():
     return
 
 def exit():
     exit()
     
 tasks = {
-    'SELECT' : select,
-    'HISTORY' : history,
-    'UPDATE' : update,
-    'DELETE' : delete,
-    'EXIT' : exit
-    }
+    1 : select,
+    2 : history,
+    3 : update,
+    4 : delete,
+    5 : exit
+}
 
 
 # The program UI
-print('name')
+
 while(True):
-    inp = input('Query: ').split()
-    tasks[inp[0]](inp)
+    print('Please choose one of the following options (enter the number):\n\
+1. SELECT\n2. HISTORY\n3. UPDATE\n4. DELETE\n5. EXIT\n')
+
+    inp = int(input('Task number: '))
+    tasks[inp]()
 # cursor.execute('select * from medicalRecord')
    
 # for row in cursor.fetchall():
