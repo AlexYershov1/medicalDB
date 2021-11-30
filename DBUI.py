9ee5c6a1506bb5f1d95ab354d4693d7b0481c3e0
yair dor <yairdor97@gmail.com>
2021-11-30 21:56:07 +0200

    all conditions done, didnt run tests yet

--- DBUI.py
+++ DBUI.py
@@ -2,7 +2,7 @@ from typing import NoReturn
 import pyodbc
 import re
 import csv
-import pandas
+#import pandas
 from datetime import datetime
 
 print('Initiated program')
@@ -138,25 +138,24 @@ def select():
     viewPointDate = input("searching at(enter 0 for now): ")
     date2 =""
     datevalid = valid_date(date)
-    if(datevalid == True):
-        if(datevalid == 'special'):
-            date = date[:-1]
-            date2 = date
-            date += "00:00"
-            date2 += "23:59"
-        else:
-            date2 = date
+    if(datevalid == 'special'):
+        date = date[:-1]
+        date2 = date
+        date += "00:00"
+        date2 += "23:59"
+    elif(datevalid == True):
+        date2 = date
     else:
         print("invalid date")
         return
 
     if (viewPointDate == '0'):
         viewPointDate = datetime.now().strftime("%d/%m/%Y %H:%M")
-    # date2
-    # if date has no hour  - date hour = 0:00 and date2 hour = 23:59
-    # if date has hour date2 hour = date hour
+    elif(viewPointDate != True):
+        print("invalid searching at date")
+        return
+ 
     # first_name, last_name, loinc, date, viewPointDate = 'Eyal', 'Rothman', '11218-5', '18-5-2018 11:00', datetime.now().strftime("%d/%m/%Y %H:%M")
-    # date2 = date
     cursor.execute('SELECT * FROM medicalRecord WHERE [First name]=? and [Last name]=? and [LOINC-NUM]=?\
         and [Valid start time] between ? and ? and [Transaction time] <= ? order by [Valid start time] desc',
                    first_name, last_name, loinc, date, date2, viewPointDate)
@@ -171,7 +170,7 @@ def select():
     index = [column[0] for column in cursor.description].index('deleted')
 
     for row in rows:
-        if(row[index] == None or row[index] < datetime.strptime(viewPointDate, "%d/%m/%Y %H:%M")):
+        if(row[index] == None or row[index] > datetime.strptime(viewPointDate, "%d/%m/%Y %H:%M")): #no way that this throw excp?
             printData(columns, row)
             return
             
@@ -183,24 +182,48 @@ def history():
     loinc = input("enter loinc-num or component : ")
     first_name = input("enter first name: ")
     last_name = input("enter last name: ")
-    dateDis = input(
-        "enter the valid date time and hour to display from, 0 hour for all day: ")
-    fromd = input(
-        "enter : transaction time range, from (0 for 1/1/1900 till now):")
+    dateDis = input("enter the valid date time and hour to display from, 0 hour for all day: ")
+    dateDis2 = ""
+    fromd = input("enter transaction time range, from (0 for 1/1/1900 till now, 0 at hour only = from 00:00):")
     if (fromd != '0'):
-        tod = input("to transaction time date and hour: ")
+        tod = input("to transaction time date and hour (enter 0 at hour for 23:59): ")
     else:
         fromd = "1/1/1900 0:00"
         tod = datetime.now().strftime("%d/%m/%Y %H:%M")
-    # dateDis2 = ????
-    # ask here if datedis contains hour , if not - set hour to 0:00 , anyway create a second datedis2 - set hour to 23:59 if no hour at
-    # datedis , if there is so datedis2 = datedis
+    if(valid_date(fromd) == False or valid_date(tod) == False):
+        print("invalid dates range")
+        return
+    elif(valid_date(fromd) == 'special'):
+        if(valid_date(tod) == 'special'):
+            tod = tod[:-1] + "23:59" # not sure thats what she wants here, do i need to print an error? 
+        fromd = fromd[:-1] + "00:00"
 
-    # check if all date inputs are valid inputs
+    validDate = valid_date(dateDis)
+    if(validDate == 'special'):
+        dateDis = dateDis[:-1]
+        dateDis2 = dateDis
+        dateDis += "00:00"
+        dateDis2 += "23:59"
+    elif(validDate == True):
+        dateDis2 = dateDis
+    else:
+        print("invalid date")
 
     cursor.execute('SELECT * FROM medicalRecord WHERE [First name]=? and [Last name]=?\
         and ([LOINC-NUM]=? or [LOINC-NUM]=?) and [Valid start time] between ? and ? and [Transaction time] between ? and ?',
-         first_name, last_name, loinc, loinc, dateDis, "18/5/2018 19:00", fromd, tod)
+         first_name, last_name, loinc, loinc, dateDis, dateDis2, fromd, tod)
+
+    rows = cursor.fetchall()
+    if (len(rows) == 0):
+        print("could not find such case that meets the criteria specified")
+        return
+    columns = [column[0] for column in cursor.description]  
+    index = [column[0] for column in cursor.description].index('deleted')
+
+    for row in rows:
+        if(row[index] == None or row[index] > datetime.strptime(tod, "%d/%m/%Y %H:%M") or row[index] < datetime.strptime(fromd, "%d/%m/%Y %H:%M")): 
+            printData(columns, row)
+            return
     return
 
 
@@ -215,6 +238,10 @@ def update():
         date = datetime.now().strftime("%d/%m/%Y %H:%M")
     elif (valid_date(date) != True):
         print("invalid date")
+        return
+    if (valid_date(validdate) != True):
+        print("invalid valid date time")
+        return
 
     #coalesce()
 
@@ -223,12 +250,13 @@ def update():
                    loinc, loinc, first_name, last_name, validdate)
 
     row = cursor.fetchone()
+     ## need to change here from row[5] to what alex did with row[9] ######################################
     if(row):
         cursor.execute('INSERT INTO medicalRecord ([First name], [Last name], [LOINC-NUM], [Value], [Unit], [Valid start time], [Transaction time])\
         values (?,?,?,?,?,?,?)', first_name, last_name, loinc, value, row[5], date, datetime.now().strftime("%d/%m/%Y %H:%M"))
         cursor.commit()
     else:
-        print("Could not Find such test to update or one of the arguments is invalid \n")
+        print("Could not Find such test to update or one of the arguments is incorrect \n")
     return
 
 
@@ -246,14 +274,13 @@ def delete():
         print("invalid date")
         return
     validDate = valid_date(valid)
-    if(validDate == True):
-        if(validDate == 'special'):
-            valid = valid[:-1]
-            valid2 = valid
-            valid += "00:00"
-            valid2 += "23:59"
-        else:
-            valid2 = valid
+    if(validDate == 'special'):
+        valid = valid[:-1]
+        valid2 = valid
+        valid += "00:00"
+        valid2 += "23:59"
+    elif(validDate == True):
+        valid2 = valid
     else:
         print("invalid date")
 
